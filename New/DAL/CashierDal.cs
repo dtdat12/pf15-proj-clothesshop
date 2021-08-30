030f5a1f84bf982b30eb8db45de373722637416c
dtdat12 <dtdat0078@gmail.com>
2021-08-30 13:34:18 +0700

    clothesshop

--- New/DAL/CashierDal.cs
+++ New/DAL/CashierDal.cs
@@ -7,27 +7,19 @@ namespace DAL
     public class CashierDal{
       public int Login(Cashier cashier){
         int login = 0;
-        // Console.WriteLine(cashier.UserName + " - " + cashier.Password);
         try{
           MySqlConnection connection = DbHelper.GetConnection();
           connection.Open();
           MySqlCommand command = connection.CreateCommand();
           command.CommandText = "select * from Cashiers where username='"+
-            cashier.UserName+"' and password='"+
-            Md5Algorithms.CreateMD5(cashier.Password)+"';";
+          cashier.UserName+"' and password='"+
+          Md5Algorithms.CreateMD5(cashier.Password)+"';";
           MySqlDataReader reader = command.ExecuteReader();
-          if(reader.Read()){
-            login = reader.GetInt32("role");
-          }else
-          {
-              login = 0;
-          }
           reader.Close();
           connection.Close();
         }catch{
           login = -1;
         }
-        // Console.WriteLine(login);
         return login;
       }
     }
