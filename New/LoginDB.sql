e7bce65a869566665df5caa3cdd47fc74ea7d775
dtdat12 <89620449+dtdat12@users.noreply.github.com>
2021-09-04 22:51:21 +0700

    Update LoginDB.sql

--- New/LoginDB.sql
+++ New/LoginDB.sql
@@ -2,16 +2,16 @@ drop database if exists LoginDB;
 create database LoginDB;
 use LoginDB;
 
-drop table customers;
-create table customers(
+drop table Customers;
+create table Customers(
 	customer_id int auto_increment primary key,
     customer_name varchar(100) not null, 
-    customer_address varchar(100) not null,
+    customer_address varchar(100),
     telephone varchar(20)
 );
 
-drop table cashiers;
-create table cashiers(
+drop table Cashiers;
+create table Cashiers(
 	cashier_id int auto_increment primary key,
     cashier_name varchar(100) not null,
     user_name varchar(100) not null unique,
@@ -19,105 +19,129 @@ create table cashiers(
     telephone varchar(20) 
 );
 
-drop table colors;
-create table colors(
+drop table Colors;
+create table Colors(
 	color_id int auto_increment primary key,
     color_name varchar(100) not null
 );
 
-drop table sizes;
-create table sizes(
+drop table Sizes;
+create table Sizes(
 	size_id int auto_increment primary key,
     size_name varchar(100) not null
 );
 
-drop table items;
-create table items(
+drop table Items;
+create table Items(
 	item_id int auto_increment primary key,
     item_name varchar(100) not null,
-    item_description varchar(200) not null unique
+    item_description varchar(100),
+    item_price decimal(18,3) default 0
 );
 
-drop table itemdetails;
-create table itemdetails(
-	quantity int not null,
-    item_id int not null,
+drop table ItemDetails;
+create table ItemDetails(
+	item_id int not null,
     color_id int not null,
     size_id int not null,
-    foreign key (item_id) references items(item_id),
-    foreign key (color_id) references colors(color_id),
-    foreign key (size_id) references sizes(size_id),
-    primary key (item_id, color_id, size_id)
+	quantity int not null default 1,
+    constraint pk_ItemDetails primary key(item_id, color_id, size_id),  
+    constraint fk_ItemDetails_Items foreign key(item_id) references Items(item_id),
+    constraint fk_ItemDetails_Colors foreign key(color_id) references Colors(color_id),
+    constraint fk_ItemDetails_Sizes foreign key(size_id) references Sizes(size_id)
 );
 
-drop table invoices;
-create table invoices(
+drop table Invoices;
+create table Invoices(
 	invoice_no int auto_increment primary key,
-    invoice_date datetime,
     cashier_id int not null,
     customer_id int not null,
-    foreign key (customer_id) references customers(customer_id),
-    foreign key (cashier_id) references cashiers(cashier_id)
-);
+	invoice_date datetime default now() not null,
+	constraint fk_Invoices_Cashiers foreign key(cashier_id) references Cashiers(cashier_id),
+	constraint fk_Invoices_Customers foreign key(customer_id) references Customers(customer_id)
+);	
 
-drop table invoicedetails;
-create table invoicedetails(
-	item_price double,
+drop table InvoiceDetails;
+create table InvoiceDetails(
+	invoice_no int not null,
     item_id int not null,
-    invoice_no int not null,
-    foreign key (invoice_no) references invoices(invoice_no),
-    foreign key (item_id) references items(item_id),
-    primary key (invoice_no, item_id)
+	item_price decimal(18,3) not null,
+    constraint pk_InvoiceDetails primary key(invoice_no, item_id),  
+    constraint fk_InvoiceDetails_Invoices foreign key(invoice_no) references Invoices(invoice_no),
+	constraint fk_InvoiceDetails_Items foreign key(item_id) references Items(item_id)
 );
 
-insert into customers values ('01', 'Khách hàng 1', 'HN', '09124124');
-insert into customers values ('02', 'Khách hàng 2', 'HP', '08234234');
-insert into customers values ('03', 'Khách hàng 3', 'VP', '07202202');
-insert into customers values ('04', 'Khách hàng 4', 'VT', '06397397');
-select * from logindb.customers;
+insert into Customers(customer_name, customer_address, telephone) values 
+		('Khach hang 1', 'Hai Duong', '09124124'),
+		('Khach hang 2', 'Hai Phong', '08234234'),
+		('Khach hang 3', 'Vinh Phuc', '07202202'),
+		('Khach hang 4', 'Vung Tau', '06397397');
+select * from Customers;
+
+insert into Colors (color_name) values
+('Black'),
+('White'),
+('Blue'),
+('Red');
+select * from Colors;
+
+insert into Sizes(size_name) values 
+('S'), ('M'), ('L'), ('XL'), ('XXL');
+select * from Sizes;
+
+insert into Items (item_name, item_description) values
+		('Áo khoác chống nắng UV', 'Bảo vệ tốt nhất - ngăn tới 99,4% tia UV được Viện Dệt May Việt Nam kiểm nghiệm và xác nhận'),
+		('Áo Polo Nam', 'Sản phẩm được thiết kế từ chất liệu cotton mềm mại, họa tiết màu sắc nổi bật, phù hợp với nhiều phom dáng và hoàn cảnh khác nhau'),
+		('Quần Khakis Nam dáng ôm', 'Phom dáng hiện đại, khỏe khoắn, cá tính thích hợp đi làm, đi chơi'),
+		('Quần Shorts Kaki Nam', 'Với thiết kế đơn giản khỏe khoắn, quần sooc phù hợp cho những hoạt động đi chơi, du lịch');
+select * from Items;
+
+insert into ItemDetails(item_id, color_id, size_id) values 
+(1, 3, 2),
+(1, 4, 3),
+(3, 2, 4),
+(4, 1, 1);
+select * from ItemDetails;
+
+insert into Invoices(invoice_no, cashier_id, customer_id) values 
+(1, 1, 3),
+(2, 1, 1),
+(3, 1, 2),
+(4, 1, 4);
+select * from Invoices;
+
+insert into InvoiceDetails(invoice_no, item_id, item_price) values 
+(1, 3, 229.000),
+(2, 4, 599.000),
+(3, 1, 111.000),
+(4, 2, 499.000);
+select * from InvoiceDetails;
 
 create user if not exists 'vtca'@'localhost' identified by 'vtcacademy';
 grant all on LoginDB.* to 'vtca'@'localhost';
 
-insert into cashiers values('001', 'Thu ngân 1', 'pf15', 'vtcacademy', '02406406');
-select * from cashiers;
-select * from cashiers where user_name='cc' and user_pass='vtcacademy';
+-- ShopClothes
+insert into Cashiers (cashier_id, cashier_name, user_name, user_pass, telephone) values
+		(1, 'shopclothes', 'Clothes', 'f637569d1f2b1af93c463b312f2d77de', '0982942754');
+select * from Cashiers;
+select * from Cashiers where user_name='Clothes' and user_pass='f637569d1f2b1af93c463b312f2d77de';
 
-insert into colors values ('1001', 'Black');
-insert into colors values ('1002', 'White');
-insert into colors values ('1003', 'Red');
-insert into colors values ('1004', 'Blue');
-select * from logindb.colors;
 
-insert into sizes values ('2001', 'S');
-insert into sizes values ('2002', 'M');
-insert into sizes values ('2003', 'L');
-insert into sizes values ('2004', 'XL');
-select * from logindb.sizes;
 
-insert into items values ('0001', 'Áo khoác chống nắng UV', 'Bảo vệ tốt nhất - ngăn tới 99,4% tia UV được Viện Dệt May Việt Nam kiểm nghiệm và xác nhận');
-insert into items values ('0002', 'Áo Polo Nam', 'Sản phẩm được thiết kế từ chất liệu cotton mềm mại, họa tiết màu sắc nổi bật, phù hợp với nhiều phom dáng và hoàn cảnh khác nhau');
-insert into items values ('0003', 'Quần Khakis Nam dáng ôm', 'Phom dáng hiện đại, khỏe khoắn, cá tính thích hợp đi làm, đi chơi');
-insert into items values ('0004', 'Quần Shorts Kaki Nam', 'Với thiết kế đơn giản khỏe khoắn, quần sooc phù hợp cho những hoạt động đi chơi, du lịch');
-select * from logindb.items;
 
-insert into itemdetails values ('10', '0001', '1003', '2001');
-insert into itemdetails values ('11', '0004', '1001', '2002');
-insert into itemdetails values ('12', '0003', '1002', '2001');
-insert into itemdetails values ('13', '0002', '1004', '2004');
-select * from logindb.itemdetails;
 
-insert into invoices values ('3001', '2021-08-27 3:36:32', '001', '01');
-insert into invoices values ('3002', '2021-08-27 3:46:32', '001', '04');
-insert into invoices values ('3003', NOW(), '001', '03');
-insert into invoices values ('3004', NOW(), '001', '02');
-select * from logindb.invoices;
 
-insert into invoicedetails values ('229.000', '0001', '1001');
-insert into invoicedetails values ('500.000', '0002', '1002');
-insert into invoicedetails values ('1.389.000', '0003', '1003');
-insert into invoicedetails values ('799.000', '0004', '1004');
-select * from logindb.invoicedetails;
+
+
+
+
+
+
+
+
+
+
+
 
 
 
