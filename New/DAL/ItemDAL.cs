b9a7d02b992c118212c3961aae0e97148b9a1a09
dtdat12 <dtdat0078@gmail.com>
2021-09-11 19:42:42 +0700

    clothesshop

---
+++ New/DAL/ItemDAL.cs
@@ -0,0 +1,70 @@
+// using System;
+// using System.Collections.Generic;
+// using MySql.Data.MySqlClient;
+// using Persistence;
+
+// namespace DAL
+// {
+//     public static class ItemFilter{
+//         public const int GET_ALL = 0;
+//         public const int FILTER_BY_ITEM_NAME = 1;
+//     }
+//     public class ItemDAL
+//     {
+//         private string query;
+//         private MySqlDataReader reader;
+//         public Item GetItemById(int itemId)
+//         {
+//             query = @"select item_id, item_name, item_price, 
+//                         ifnull(item_description, '') as item_description
+//                         from Items where item_id=" + itemId + ";";
+//             DBHelper.OpenConnection();
+//             reader = DBHelper.ExecQuery(query);
+//             Item item = null;
+//             if (reader.Read())
+//             {
+//                 item = GetItem(reader);
+//             }
+//             DBHelper.CloseConnection();
+//             return item;
+//         }
+//         private Item GetItem(MySqlDataReader reader)
+//         {
+//             Item item = new Item();
+//             item.ItemId = reader.GetInt32("item_id");
+//             item.ItemName = reader.GetString("item_name");
+//             item.ItemPrice = reader.GetDecimal("unit_price");
+//             item.ItemDescription = reader.GetString("item_description");
+//             return item;
+//         }
+//         private List<Item> GetItems(MySqlCommand command)
+//         {
+//             List<Item> lstItem = new List<Item>();
+//             reader = command.ExecuteReader();
+//             while (reader.Read())
+//             {
+//                 lstItem.Add(GetItem(reader));
+//             }
+//             reader.Close();
+//             DBHelper.CloseConnection();
+//             return lstItem;
+//         }
+//         public List<Item> GetItems(int itemFilter, Item item)
+//         {
+//             MySqlCommand command = new MySqlCommand("", DBHelper.OpenConnection());
+//             switch(itemFilter)
+//             {
+//                 case ItemFilter.GET_ALL:
+//                     query = @"select item_id, item_name, item_price, ifnull(item_description, '') as item_description from Items";
+//                     break;
+//                 case ItemFilter.FILTER_BY_ITEM_NAME:
+//                     query = @"select item_id, item_name, item_price, ifnull(item_description, '') as item_description from Items
+//                                 where item_name like concat('%',@itemName,'%');";
+//                     command.Parameters.AddWithValue("@itemName", item.ItemName);
+//                     break;
+//             }
+//             command.CommandText = query;
+//             return GetItems(command);
+//         }
+//     }
+// }
